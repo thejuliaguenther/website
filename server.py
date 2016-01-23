6186aae79e7ee0fc8c831d8f0467fb180c45130c
Julia Guenther <juliag1@alumni.stanford.edu>
2016-01-23 12:10:15 -0800

    Working on routing

--- server.py
+++ server.py
@@ -1,19 +1,27 @@
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, make_response, redirect, request, flash, session
 
 app = Flask(__name__)
 app.secret_key = "HyrVFG2jcVlpN0qH"
 
 app.jinja_env.undefined = StrictUndefined
 
-@app.route('/', methods=["GET"])
-def index():
-    """
-    Renders the main page, index.html, from which all routes are 
-    served using Angular.js
-    """
-    return render_template("index.html")
+# @app.route('/', methods=["GET"])
+# def index():
+#     """
+#     Renders the main page, index.html, from which all routes are 
+#     served using Angular.js
+#     """
+#     return render_template("index.html")
+
+@app.route('/')
+@app.route('/about')
+@app.route('/projects')
+@app.route('/contact')
+def render_pages(**kwargs):
+    return make_response(open('templates/index.html').read())
+
 
 if __name__ == "__main__":
     
